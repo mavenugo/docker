4b4de5d07ffa48bf7551fd73e34bf41756d6a9ca
Madhu Venugopal <madhu@docker.com>
2016-10-23 00:10:16 -0700

    Libnetwork vendoring for "windows" ipam driver
    Signed-off-by: Madhu Venugopal <madhu@docker.com>

--- vendor/src/github.com/docker/libnetwork/ipams/builtin/builtin_windows.go
+++ vendor/src/github.com/docker/libnetwork/ipams/builtin/builtin_windows.go
@@ -1,3 +1,5 @@
+// +build windows
+
 package builtin
 
 import (
@@ -7,10 +9,12 @@ import (
 	"github.com/docker/libnetwork/ipam"
 	"github.com/docker/libnetwork/ipamapi"
 	"github.com/docker/libnetwork/ipamutils"
+
+	windowsipam "github.com/docker/libnetwork/ipams/windowsipam"
 )
 
-// Init registers the built-in ipam service with libnetwork
-func Init(ic ipamapi.Callback, l, g interface{}) error {
+// InitDockerDefault registers the built-in ipam service with libnetwork
+func InitDockerDefault(ic ipamapi.Callback, l, g interface{}) error {
 	var (
 		ok                bool
 		localDs, globalDs datastore.DataStore
@@ -38,4 +42,16 @@ func Init(ic ipamapi.Callback, l, g interface{}) error {
 	cps := &ipamapi.Capability{RequiresRequestReplay: true}
 
 	return ic.RegisterIpamDriverWithCapabilities(ipamapi.DefaultIPAM, a, cps)
+}
+
+// Init registers the built-in ipam service with libnetwork
+func Init(ic ipamapi.Callback, l, g interface{}) error {
+	initFunc := windowsipam.GetInit(windowsipam.DefaultIPAM)
+
+	err := InitDockerDefault(ic, l, g)
+	if err != nil {
+		return err
+	}
+
+	return initFunc(ic, l, g)
 }
